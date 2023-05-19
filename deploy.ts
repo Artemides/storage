d2bce870f250d22371dcc9d6fe3bd9eed62e48f5
Artemides <edmpulasky@gmail.com>
2023-05-18 22:09:30 -0500

    load compiled contract

--- deploy.ts
+++ deploy.ts
@@ -4,4 +4,13 @@ const main = async () => {
   const { Wallet, JsonRpcProvider } = ethers;
   const provider = new JsonRpcProvider(process.env.GANACHE_URL);
   const wallet = new Wallet(process.env.ACCOUNT_PRIVATE_KEY!, provider);
+
+  const contractAbi = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.abi",
+    "utf8"
+  );
+  const contractBinaty = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
 };
