6be9f9a418b43b24b4af9c75d758626968b85c91
Artemides <edmpulasky@gmail.com>
2023-05-18 22:19:13 -0500

    set up dotenv vars

--- deploy.ts
+++ deploy.ts
@@ -1,7 +1,8 @@
 import { ethers } from "ethers";
 import fs from "fs-extra";
+import "dotenv/config";
 const main = async () => {
-  const { Wallet, JsonRpcProvider } = ethers;
+  const { Wallet, JsonRpcProvider, ContractFactory } = ethers;
   const provider = new JsonRpcProvider(process.env.GANACHE_URL);
   const wallet = new Wallet(process.env.ACCOUNT_PRIVATE_KEY!, provider);
 
@@ -9,8 +10,19 @@ const main = async () => {
     "./SimpleStorage_sol_SimpleStorage.abi",
     "utf8"
   );
-  const contractBinaty = fs.readFileSync(
+  const contractBinary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
     "utf8"
   );
+
+  const contractFactory = new ContractFactory(
+    contractAbi,
+    contractBinary,
+    wallet
+  );
+
+  const contract = await contractFactory.deploy();
+  console.log({ contract });
 };
+
+main();
